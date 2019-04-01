55f2f83b88440297778011ee49b872cc11825709
Szymon Szczpeański <szymon.szczepanski@gmail.com>
2019-04-02 01:59:52 +0200

    spliter panel

--- Controls/Panels/Form1.Designer.cs
+++ Controls/Panels/Form1.Designer.cs
@@ -28,76 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.groupBox2.SuspendLayout();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // splitContainer1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(250, 116);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Mój tyuł";
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
             // 
-            // groupBox2
+            // splitContainer1.Panel1
             // 
-            this.groupBox2.Controls.Add(this.radioButton2);
-            this.groupBox2.Controls.Add(this.radioButton1);
-            this.groupBox2.Location = new System.Drawing.Point(356, 28);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 100);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Red;
             // 
-            // radioButton1
+            // splitContainer1.Panel2
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(6, 21);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(110, 21);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(7, 49);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(110, 21);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.Blue;
+            this.splitContainer1.Size = new System.Drawing.Size(767, 411);
+            this.splitContainer1.SplitterDistance = 255;
+            this.splitContainer1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(767, 411);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- Controls/Panels/Form1.cs
+++ Controls/Panels/Form1.cs
@@ -15,6 +15,7 @@ namespace Panels
         public Form1()
         {
             InitializeComponent();
+            this.splitContainer1.SplitterDistance = 85;
 
             //for (int i = 0; i < 100; i++)
             //{
@@ -26,7 +27,22 @@ namespace Panels
             //    });
             //}
 
-            
+            Timer timer=new Timer();
+            timer.Interval = 10;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private int direction = 1;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var width = this.splitContainer1.Width;
+            if(this.splitContainer1.SplitterDistance+2>=this.splitContainer1.Width-80)
+                direction = direction * -1;
+            if (this.splitContainer1.SplitterDistance - 2 <= 80)
+                direction = direction * -1;
+
+            this.splitContainer1.SplitterDistance = this.splitContainer1.SplitterDistance +(direction)* 2;
         }
     }
 }
