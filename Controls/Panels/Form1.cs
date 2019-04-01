d29eb1d758050b19ed33ee698adb0348fe0bb171
Szymon Szczpeański <szymon.szczepanski@gmail.com>
2019-04-02 01:43:15 +0200

    flowpanel

--- Controls/Panels/Form1.cs
+++ Controls/Panels/Form1.cs
@@ -15,6 +15,18 @@ namespace Panels
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+            {
+                this.flowLayoutPanel1.Controls.Add(new Button
+                {
+                    Name = "btn"+i,
+                    Size = new Size(30,30),
+                    Text = i.ToString()
+                });
+            }
+
+            this.flowLayoutPanel1.FlowDirection = FlowDirection.BottomUp;
         }
     }
 }
