21712f9597f7f9124fd7493e5eabbef76811029c
Szymon Szczpeański <szymon.szczepanski@gmail.com>
2019-04-02 01:24:07 +0200

    init

---
+++ Controls/ScrollingControls/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScrollingControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 100; i++)
+            {
+                panel1.Controls.Add(new Button
+                {
+                    Name = "btn"+i,
+                    Location = new Point(0,i*30),
+                    Text=i.ToString()
+                });
+            }
+        }
+    }
+}
