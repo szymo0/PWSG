f0cdc6928c643837156d46e70ede0c5049d41e4e
Szymon Szczepanski <szymon.szczepanski@dxc.com>
2019-04-02 12:23:18 +0200

    simple links

---
+++ Controls/CommonControls/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommonControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "PWSG label";
+            linkLabel1.Text = "Link to PWSG";
+            linkLabel1.Links.Add(0, 10);
+        }
+    }
+}
