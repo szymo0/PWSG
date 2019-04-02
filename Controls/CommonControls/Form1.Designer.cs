10d39fea3ee14ba87e78e3ddf5dcc776aa344c4b
Szymon Szczepanski <szymon.szczepanski@dxc.com>
2019-04-02 13:26:57 +0200

    checkboxList end

--- Controls/CommonControls/Form1.Designer.cs
+++ Controls/CommonControls/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.fontDialog1 = new System.Windows.Forms.FontDialog();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -45,22 +46,35 @@
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.Size = new System.Drawing.Size(209, 94);
             this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.CheckedListBox1_SelectedIndexChanged);
+            this.checkedListBox1.SelectedValueChanged += new System.EventHandler(this.CheckedListBox1_SelectedValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 159);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 185);
+            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(449, 356);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkedListBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.FontDialog fontDialog1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Controls/CommonControls/Form1.cs
+++ Controls/CommonControls/Form1.cs
@@ -16,9 +16,26 @@ namespace CommonControls
         public Form1()
         {
             InitializeComponent();
+            checkedListBox1.CheckOnClick = true;
       
         }
 
-   
+        private void CheckedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Text = "Wybrałeś " + ((string)checkedListBox1.SelectedItem);
+        }
+
+        private void CheckedListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder("Do załatwienia pozostało:");
+            stringBuilder.AppendLine();
+            foreach(var item in checkedListBox1.Items)
+            {
+                if (checkedListBox1.CheckedItems.Contains(item)) continue;
+                stringBuilder.AppendLine(" - "+ item.ToString());
+            }
+
+            textBox1.Text = stringBuilder.ToString();
+        }
     }
 }
